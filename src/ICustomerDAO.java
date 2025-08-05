29a75f225f8a4adc9e667af77248ded1729255f0
Gelila-Fassil <gelilafassil659@gmail.com>
2025-08-05 10:13:17 +0300

    updated ICustomerDAO oage

--- src/ICustomerDAO.java
+++ src/ICustomerDAO.java
@@ -1,49 +1,21 @@
 import java.util.List;
 
-/**
- * Interface for Customer Data Access Object (DAO) operations.
- * Defines the contract for interacting with the 'customers' table.
- */
 public interface ICustomerDAO {
-    /**
-     * Adds a new customer to the database.
-     * @param customer The Customer object to add.
-     * @return true if the customer was added successfully, false otherwise.
-     */
+    // Add a new customer
     boolean addCustomer(Customer customer);
 
-    /**
-     * Retrieves a customer by their ID.
-     * @param customerId The ID of the customer to retrieve.
-     * @return The Customer object if found, null otherwise.
-     */
+    // Get customer by ID
     Customer getCustomerById(int customerId);
 
-    /**
-     * Retrieves all customers from the database.
-     * @return A List of Customer objects.
-     */
+    // Get all customers
     List<Customer> getAllCustomers();
 
-    /**
-     * Updates an existing customer record in the database.
-     * @param customer The Customer object with updated details.
-     * @return true if the customer was updated successfully, false otherwise.
-     */
+    // Update customer details
     boolean updateCustomer(Customer customer);
 
-    /**
-     * Deletes a customer record from the database based on their ID.
-     * @param customerId The ID of the customer record to delete.
-     * @return true if the customer was deleted successfully, false otherwise.
-     */
+    // Delete customer by ID
     boolean deleteCustomer(int customerId);
 
-    /**
-     * Retrieves a customer by their first name and last name.
-     * @param firstName The first name of the customer.
-     * @param lastName The last name of the customer.
-     * @return The Customer object if found, null otherwise.
-     */
+    // Get customer by first and last name
     Customer getCustomerByName(String firstName, String lastName);
 }
